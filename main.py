3e13585b6a7d63bda89800639d47cbb71eba8c6b
uazamula <68710566+uazamula@users.noreply.github.com>
2023-12-20 09:19:57 +0000

    flask+page

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', username=["username"])
+
+
+@app.route("/user/<int: id>")
+def user(id):
+    username = request.args.get("username")
+    return f"{username}. {id}"
 
 
 if __name__ == '__main__':
